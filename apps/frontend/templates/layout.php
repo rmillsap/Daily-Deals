d7f5f1683eb4d20d1371099cf8f8ed51b5d1389e
Robby <robby@missionsolutions.com>
2011-10-11 14:54:51 -0700

    Latest updates

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
     <?php include_http_metas() ?>
@@ -9,6 +9,44 @@
     <?php include_javascripts() ?>
   </head>
   <body>
+
+      <header class="block span-24 shadow">
+          <div class="logo">Daily Deals</div>
+          <nav>
+              <ul>
+                  <li class="selected round">
+                      <?php echo link_to("Featured Deals", "/") ?>
+                  </li>
+                  <li>
+                      <?php echo link_to("All Deals", "/search") ?>
+                  </li>
+              </ul>
+          </nav>
+      </header>
+
+  <div class="container column span-24">
+
+      <div class="block">
+          <div class="column span-18 content-box left-column shadow">
+              Deals feed...
+          </div>
+
+          <div class="column span-4 right-column">
+              
+              <div class="content-box shadow">
+                  <h1>More Deals</h1>
+
+              </div>
+
+          </div>
+
+      </div>
+
+
+
+  </div>
+
     <?php echo $sf_content ?>
+
   </body>
 </html>
